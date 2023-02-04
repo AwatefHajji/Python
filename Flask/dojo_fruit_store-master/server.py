572354f3a4ceba60acef023eccef7cc2c1d41c9f
Awatef Hajji <awatefhajji5@gmail.com>
2023-02-04 10:52:48 +0100

    dojo_fruit_store-master_Assig

---
+++ Flask/dojo_fruit_store-master/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    count = int(request.form['strawberry'])+int(request.form['apple'])+int(request.form['raspberry'])
+    print(f"Charging {request.form['first_name']} for {count} fruits")
+    print(request.form)
+    return render_template("checkout.html")
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
